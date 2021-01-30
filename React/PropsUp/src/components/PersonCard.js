ef1cbc29c56a57b66c4f0ae6f07d5221549f3f5d
Nidal <nidaldannoon@gmail.com>
2021-01-30 17:13:00 +0200

    update

--- React/PropsUp/src/components/PersonCard.js
+++ React/PropsUp/src/components/PersonCard.js
@@ -1,12 +1,12 @@
-import React,{ Component } from 'react'
+import React,{Component} from 'react'
 class PersonCard extends Component {
     render() {
-        const { firstName,lastName,age,color } = this.props;
+        const {firstName,lastName,age,color} = this.props;
         return (
             <div>
-                <h1>{ lastName},{ firstName }</h1>
-                <h3>Age: { age }</h3>
-                <h3>Hair Color: { color }</h3>
+                <h1>{lastName},{firstName}</h1>
+                <h3>Age: {age}</h3>
+                <h3>Hair Color: {color}</h3>
             </div>
         );
     }
