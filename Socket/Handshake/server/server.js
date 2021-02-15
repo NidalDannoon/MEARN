bec0d653c0cdef4b4279d0e2a33e8f8498d12582
NidalDannoon <nidaldannoon@gmail.com>
2021-02-15 09:55:05 +0200

    update

--- Socket/Handshake/server/server.js
+++ Socket/Handshake/server/server.js
@@ -11,6 +11,6 @@ const io = require('socket.io')(server, { cors: true });
 
 io.on("connection",socket=>{
     console.log("Nice to meet you. (shake hand)")
-    socket.emit("Welcome",{message:"Welcom!"});
+    socket.emit("Welcome",{message:"Welcome!"});
     
 });
